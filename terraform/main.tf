3be50d6d07a7e3bd1f06b2cf560b818f6abcc91b
Munachi Ernest-Eze <munachi@ekogenie.com>
2025-09-08 03:09:43 -0400

    ci(aks): avoid Terraform roleAssignments by ensuring AcrPull via CLI; grant VM MI UA Admin; remove TF azurerm_role_assignment

--- terraform/main.tf
+++ terraform/main.tf
@@ -147,13 +147,6 @@ resource "azurerm_kubernetes_cluster" "main" {
   }
 }
 
-# Grant AKS access to ACR
-resource "azurerm_role_assignment" "aks_acr" {
-  principal_id                     = azurerm_kubernetes_cluster.main.kubelet_identity[0].object_id
-  role_definition_name            = "AcrPull"
-  scope                          = azurerm_container_registry.main.id
-  skip_service_principal_aad_check = true
-}
 
 # Application Gateway (for ingress)
 resource "azurerm_public_ip" "gateway" {
